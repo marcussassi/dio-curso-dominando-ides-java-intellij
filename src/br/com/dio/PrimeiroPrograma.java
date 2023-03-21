36bcd669681bd4314826467ee777966f0fe328b6
marcussassi <marcussassicesar@gmail.com>
2023-03-20 21:02:06 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+         int a = 5;
+         int b = 6;
+        System.out.println("Hello, Dio & BancoPan " + (a+b));
+    }
+}
