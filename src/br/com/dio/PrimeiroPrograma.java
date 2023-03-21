c959313ebf2aef164cae63245afe2998d8456499
marcussassi <marcussassicesar@gmail.com>
2023-03-20 22:16:19 -0300

    Criado Classe Gato

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,9 +1,14 @@
 package br.com.dio;
 
+import br.com.dio.model.Gato;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-         int a = 5;
+        /* int a = 5;
          int b = 6;
-        System.out.println("Hello, Dio & BancoPan " + (a+b));
+        System.out.println("Hello, Dio & BancoPan " + (a+b));*/
+
+        Gato gato = new Gato();
+        System.out.println(gato);
     }
 }
